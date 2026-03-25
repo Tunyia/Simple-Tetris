c23c665c4d0a179ecd04f3f04fb7fcdcedcc6052
Tunya <164085895+Tunyia@users.noreply.github.com>
2026-03-25 22:38:28 +0300

    using main.py can now be run menu.py and through menu run tetris.py and network.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+from menu import choose_mode
 from tetris import run_game
 
-run_game(True) #True - поле игрока и поле оппонента. False - только одно поле
+mode_data = choose_mode()
+
+mode = mode_data["mode"]
+ip = mode_data["ip"]
+
+if mode == "single":
+    run_game(multiplayer=False)
+
+elif mode == "host":
+    run_game(multiplayer=True)
+
+elif mode == "join":
+    run_game(multiplayer=True)
+
+else:
+    print("Exit")
